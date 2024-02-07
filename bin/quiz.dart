166be3f4352fc3c98ec991951396c7b1b4015dec
Abu Usamah <abuusamahabdurrahman@gmail.com>
2024-02-07 10:05:22 +0700

    kommit 070220241

--- bin/quiz.dart
+++ bin/quiz.dart
@@ -4,27 +4,76 @@ import 'dart:io';
 void menu() {
   print("Menu Kalkulator :");
   print("1 Penambahan");
-  print("2 Pengurangan");
+  print("2 Perkalian");
   print("3 Menghitung discount");
   print("4 Menulis nama lengkap");
   print("5 Keluar");
   print("Silahkan pilih menu dengan ketik nomer");
   int? nomer = int.parse(stdin.readLineSync()!);
 
-  main(nomer);
+  switch(nomer) {
+    case 1:
+      penambahan();
+      break;
+    case 2:
+      perkalian();
+      break;
+    case 3:
+      discount();
+      break;
+    case 4:
+      fullName();
+      break;
+    case 5:
+      print("sampai jumpa");
+      exit(0);
+    default:
+      print("Menu tidak tersedia");
+      break;
+  }
+
+  menu();
 }
 
 void penambahan() {
   print("menu 1 penambahan");
   print("tulis angka penambahan pertama");
   int? n1 = int.parse(stdin.readLineSync()!);
-
   print("Tulis angka penambahan kedua");
   int? n2 = int.parse(stdin.readLineSync()!);
-
   int sum = n1 + n2;
   print("Sum is $sum");
   menu();
+}
+
+void perkalian() {
+  print("Menu 2 Perkalian");
+  print("tulis angka perkalian ke 1");
+  int? np1 = int.parse(stdin.readLineSync()!);
+  print("tulis angka perkalian ke 2");
+  int? np2 = int.parse(stdin.readLineSync()!);
+  int kali = np1 * np2;
+  print("hasil perkalian = $kali");
+  menu();
+}
+
+void discount() {
+  print("Menu 3 Menghitung discount");
+  print("isi harga barang");
+  int? harga = int.parse(stdin.readLineSync()!);
+  print("isi diskon (%)");
+  int? diskon = int.parse(stdin.readLineSync()!);
+  int hargaDiskon = harga - (harga * diskon ~/ 100);
+  print("harga setelah diskon adalah : $hargaDiskon");
+}
+
+void fullName() {
+  print("Menu 4 Menulis nama Lengkap");
+  print("tulis nama awal");
+  String? firstName = stdin.readLineSync();
+  print("tulis nama selanjutnya");
+  String? lastName = stdin.readLineSync();
+  print("nama lengkap anda adalah $firstName $lastName");
 }
  
 void main(nomer) {
@@ -41,39 +90,4 @@ void main(nomer) {
     } else {
       print("Input tidak valid");
     }
-
-    
-
-    // print("tulis angka penambahan pertama");
-    // int? n1 = int.parse(stdin.readLineSync()!);
- 
-    // print("Tulis angka penambahan kedua");
-    // int? n2 = int.parse(stdin.readLineSync()!);
- 
-    // int sum = n1 + n2;
-    // print("Sum is $sum");
-
-    // print("menu 2 perkalian");
-    // print("tulis angka perkalian ke 1");
-    // int? np1 = int.parse(stdin.readLineSync()!);
-    // print("tulis angka perkalian ke 2");
-    // int? np2 = int.parse(stdin.readLineSync()!);
-    // int kali = np1 * np2;
-    // print("hasil perkalian = $kali");
-
-    // print("menu 3 menghitung harga diskon");
-    // print("isi harga barang");
-    // int? harga = int.parse(stdin.readLineSync()!);
-    // print("isi diskon");
-    // int? diskon = int.parse(stdin.readLineSync()!);
-    // int hargaDiskon = harga * diskon ~/ 100;
-    // print("harga setelah diskon adalah : $hargaDiskon");
-
-    // print("menulis nama lengkap");
-    // print("tulis nama awal");
-    // String? firstName = stdin.readLineSync();
-    // print("tulis nama selanjutnya");
-    // String? lastName = stdin.readLineSync();
-    // print("nama lengkap anda adalah $firstName $lastName");
-
 }
